f5ecf09491b2454434cb2094f37178356a38c4d9
lugaz <103002336+lugaz@users.noreply.github.com>
2024-01-15 19:42:33 -0500

    TEST

---
+++ L1/1printLesson.py
@@ -0,0 +1,21 @@
+# print lesson
+
+print("hello TDM 511")
+print("\n")
+print ("Welcome to lab two")
+
+name = "Lucas Ugaz"
+Major = "Computer Science"
+
+print (name)
+print (Major)
+print ("Name: ",name)
+
+
+
+# Question 1:
+# Write code to print your name on the screen using the print statement.
+# Your code:
+
+# Question 2:
+# Create a print statement that displays the result of adding two numbers, e.g., 5 + 7.
---
+++ L1/2_If_else.py
@@ -0,0 +1,31 @@
+# if else lesson
+
+a = 1
+b = 1
+c = 3
+d = 4
+e = 5
+
+if a != b:
+    print("not equal")
+else:
+    print("equal")
+
+
+# Question 1
+# Create a program that takes a student's 
+# score as input and uses an if-else statement to determine their letter grade.
+# Assume the grading scale is as follows:
+
+# A: 90-100
+# B: 80-89
+# C: 70-79
+# D: 60-69
+# F: Below 60
+# The program should print the corresponding letter grade.
+
+# Question 2
+# Write a Python program that asks the user to enter
+# an integer number. Use an if-else statement to 
+# check if the number is even or odd. Print a 
+# message indicating whether the number is even or odd.
