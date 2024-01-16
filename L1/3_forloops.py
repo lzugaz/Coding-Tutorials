29469e3c36b1cd5b1c6692abd6d50ca6e67c903f
lugaz <103002336+lugaz@users.noreply.github.com>
2024-01-16 08:56:06 -0500

    L1

---
+++ L1/3_forloops.py
@@ -0,0 +1,61 @@
+
+for i in range(10):
+    print(i)
+
+
+
+
+
+# Question 1:
+#Write a program that uses nested for loops to print the following pattern:
+
+
+# *
+# **
+# ***
+# ****
+# *****
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Number of rows for the pattern
+# rows = 5
+
+# Outer loop to iterate through rows
+# for i in range(1, rows + 1):
+#     # Inner loop to print '*' for each column
+#     for j in range(i):
+#         print("*", end="")
+#     # Move to the next line after each row
+#     print()
